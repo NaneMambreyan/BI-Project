97530e72efc594c30c18701e96ebfe030a3e3b2d
Nane Mambreyan <mambreyannane@gmail.com>
2024-05-16 23:08:47 +0400

    tasks.py

--- Pipeline_dimensional_data/config.py
+++ Pipeline_dimensional_data/config.py
@@ -1,5 +1,4 @@
 def choosing_input_dir(task_type):
-<<<<<<< HEAD
     if task_type == "create_table":
         input_dir = 'C:\\Users\\USER\\Desktop\\BI-Project\\infrastructure_initiation'
     elif task_type == "update":
@@ -8,13 +7,3 @@ def choosing_input_dir(task_type):
 
 sql_server_config = 'C:\\Users\\USER\\Desktop\\BI-Project\\infrastructure_initiation\\sql_server_config.cfg'
 print('---------------DIMENSIONAL config.py done----------------------')
-=======
-    if task_type in ["create_table"]:
-        input_dir = "C:\\Users\\Areg\\Desktop\\BI-Project\\BI-Project\\infrastructure_initiation"
-    elif task_type == "insert_data":
-        input_dir = "C:\\Users\\Areg\\Desktop\\BI-Project\\BI-Project\\Pipeline_dimentional_data\\queries"
-    return input_dir
-
-sql_server_config = "C:\\Users\\Areg\\Desktop\\BI-Project\\BI-Project\\infrastructure_initiation\\sql_server_config.cfg"
-print('---------------config.py done----------------------')
->>>>>>> b2fdc68c70a3950b3d2ffbe80a39928843e4f5a4
